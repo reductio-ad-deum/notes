f269f4c9cd0b24d9657fa2340dfc9170b8036170
Reductio ad Deum <reductio-ad-deum@protonmail.com>
2025-07-18 09:07:11 -0400

    Favicon implementation

--- quartz.config.ts
+++ quartz.config.ts
@@ -78,6 +78,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
+	  Plugin.Favicon(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
